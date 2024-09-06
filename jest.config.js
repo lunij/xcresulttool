39874ed60959e65d1902b5cce0be611e18b90579
Marc Schultz <lunij@users.noreply.github.com>
2024-09-06 13:31:00 +0200

    Ensure Jest runs in ES mode

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
-module.exports = {
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
+  },
   clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testTimeout: 60000,
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
   verbose: true
 }
